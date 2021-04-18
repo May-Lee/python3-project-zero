257cbc9ff9c67e07392d36bf4aed29556709d2fd
May-Lee Sia <mayleesia@gmail.com>
2021-04-18 12:57:22 +0200

    Initialize Database and Marshmallow

--- flask-traversy/app.py
+++ flask-traversy/app.py
@@ -11,6 +11,10 @@ basedir = os.path.abspath(os.path.dirname(__file__)) # create file path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
+# Init DB and Marshmallow (serialization, deserialization, validation etc.)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
 # Run server
 if __name__ == '__main__':
     app.run(debug=True) # debug for development
