e8da5c217c98de1f6196fa50204573e82ae8450d
May-Lee Sia <mayleesia@gmail.com>
2021-04-19 09:18:02 +0200

    Add first model

--- flask-traversy/app.py
+++ flask-traversy/app.py
@@ -15,6 +15,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# Create a class (model)
+class ExtensionURI(db.Model):
+    '''
+    This class definition uses vocabulary from Tahoe-LAFS but is not functionally
+    similar. I use these terms to begin an inner dialogue with myself in this example.
+    In Tahoe-LAFS, the URI extension is a collection of metadata about a share.
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    codec_name = db.Column(db.String(100))
+    codec_params = db.Column(db.String(100))
+    crypttext_hash = db.Column(db.String(100), unique=True)
+    crypttext_root_hash = db.Column(db.String(100), unique=True)
+    needed_shares = db.Column(db.Integer)
+    num_segments = db.Column(db.Integer)
+    segment_size = db.Column(db.Integer)
+    share_root_hash = db.Column(db.String(100), unique=True)
+    size = db.Column(db.Integer)
+    tail_codec_params = db.Column(db.String(100))
+    # TODO: learn to use None for absent parameters
+    def __ini__(self, codec_name, codec_params, crypttext_hash, crypttext_root_hash,
+                needed_shares, num_segments, segment_size, share_root_hash, size, 
+                tail_codec_params):
+        self.codec_name = codec_name
+        self.codec_params = codec_params
+        self.crypttext_hash = crypttext_hash
+        self.crypttext_root_hash = crypttext_root_hash
+        self.needed_shares = needed_shares
+        self.num_segments = num_segments
+        self.segment_size = segment_size
+        self.share_root_hash = share_root_hash
+        self.size = size
+        self.tail_codec_params = tail_codec_params
+
 # Run server
 if __name__ == '__main__':
     app.run(debug=True) # debug for development
