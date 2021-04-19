97fcef32d26c06444a693a3e525c41953d6c2e3d
May-Lee Sia <mayleesia@gmail.com>
2021-04-19 12:57:06 +0200

    Add schema

--- flask-traversy/app.py
+++ flask-traversy/app.py
@@ -54,6 +54,9 @@ class ExtensionSchema(ma.Schema):
         fields = ('id', 'codec_name', 'codec_params', 'crypttext_hash', 'crypttext_root_hash',
                   'needed_shares', 'num_segments', 'segment_size', 'share_root_hash', 'size'
                   'tail_codec_params')
+# Init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
 
 # Run server
 if __name__ == '__main__':
